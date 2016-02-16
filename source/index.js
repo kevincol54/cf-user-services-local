64b1b38cb3851a278488c688937cfb6b16fb4ac9
Kevin <kevincol54@gmail.com>
2016-02-16 12:55:20 -0500

    refactor, add standard, code cleanup

--- source/index.js
+++ source/index.js
@@ -1,17 +1,16 @@
-const {compose, keys} = require('ramda')
+const {keys} = require('ramda')
 
 const getCredentials = (userService) => {
-  const appEnv = require("cfenv").getAppEnv();
-  const config_services = appEnv.getServices();
+  const appEnv = require('cfenv').getAppEnv()
+  const config_services = appEnv.getServices()
   return (keys(config_services).length > 0) ? config_services[userService].credentials : undefined
 }
 
-const creds = (userService, defaultCreds) => userService ? userService : defaultCreds
-
 module.exports = (input) => {
   let returnObj = {}
-  keys(input).map((x) => {
-    returnObj[x] = creds(getCredentials(x), input[x])
+  keys(input).map((serviceName) => {
+    const userService = getCredentials(serviceName)
+    returnObj[serviceName] = userService || input[serviceName]
   })
   return returnObj
 }
