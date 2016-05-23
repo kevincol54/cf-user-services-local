f4f8fc2c34e069de1ccf9806861c900a439015af
Kevin <kevincol54@gmail.com>
2016-05-23 14:55:00 -0400

    removing debug logs

--- source/index.js
+++ source/index.js
@@ -3,15 +3,12 @@ const appEnv = require('cfenv').getAppEnv()
 const configServices = appEnv.getServices()
 
 const getCredentials = (userService) => {
-  console.log("userService", userService);
-  console.log("configServices[userService]", configServices[userService]);
-  return (keys(configServices).length > 0) ? ( (configServices[userService]) ? configServices[userService].credentials : undefined) : undefined
+  return (keys(configServices).length > 0) ? ( (configServices[userService]) ? configServices[userService].credentials : undefined ) : undefined
 }
 
 module.exports = (input) => {
   let returnObj = {}
   keys(input).map((serviceName) => {
-    console.log("serviceName", serviceName);
     returnObj[serviceName] = getCredentials(serviceName) || input[serviceName]
   })
   return returnObj
