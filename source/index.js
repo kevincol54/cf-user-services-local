452ecbb8c5c982a66883a035d32425f7a11ec370
Kevin <kevincol54@gmail.com>
2016-05-23 14:24:12 -0400

    debug version

--- source/index.js
+++ source/index.js
@@ -1,14 +1,17 @@
-const {keys} = require('ramda')
+const { keys } = require('ramda')
 const appEnv = require('cfenv').getAppEnv()
 const configServices = appEnv.getServices()
 
 const getCredentials = (userService) => {
+  console.log("userService", userService);
+  console.log("configServices[userService]", configServices[userService]);
   return (keys(configServices).length > 0) ? configServices[userService].credentials : undefined
 }
 
 module.exports = (input) => {
   let returnObj = {}
   keys(input).map((serviceName) => {
+    console.log("serviceName", serviceName);
     returnObj[serviceName] = getCredentials(serviceName) || input[serviceName]
   })
   return returnObj
