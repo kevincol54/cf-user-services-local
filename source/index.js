ad623bd91bdc6bb1e9f616cf9db213f3fce6be0b
Mark Hudson <mhudson@trhc.com>
2018-12-27 15:02:32 -0500

    Update initialization to merge defaults with UPS

--- source/index.js
+++ source/index.js
@@ -1,15 +1,15 @@
-const { keys } = require('ramda')
+const { keys, mergeRight } = require('ramda')
 const appEnv = require('cfenv').getAppEnv()
 const configServices = appEnv.getServices()
 
 const getCredentials = (userService) => {
-  return (keys(configServices).length > 0) ? ( (configServices[userService]) ? configServices[userService].credentials : undefined ) : undefined
+  return (keys(configServices).length > 0) ? ((configServices[userService]) ? configServices[userService].credentials : undefined) : undefined
 }
 
 module.exports = (input) => {
   let returnObj = {}
   keys(input).map((serviceName) => {
-    returnObj[serviceName] = getCredentials(serviceName) || input[serviceName]
+    returnObj[serviceName] = mergeRight(input[serviceName], getCredentials(serviceName))
   })
   return returnObj
 }
--- test/index.js
+++ test/index.js
@@ -1,22 +1,26 @@
 const test = require('tap').test
 const rewire = require('rewire')
 const buildConfigs = require('../distribution')
-const mockInput = {
+const defaultValues = {
   'couchdb': {
     username: 'admin',
     password: 'admin',
     host: 'localhost',
-    port: '5984'
+    port: '5984',
+    url: 'http://notInUpsDefinition'
   },
   'mysql': {
     host: '127.0.0.1',
-    user: 'root',
+    username: 'root',
     password: '',
     database: 'foo_db'
+  },
+  'no-service': {
+    key: 'value'
   }
 }
 
-const expectedOutput = {
+const upsDefinitions = {
   'couchdb': {
     host: 'couchdb-develop.net',
     password: 'password',
@@ -35,23 +39,47 @@ const expectedOutput = {
   }
 }
 
+const expectedOutput = {
+  'couchdb': {
+    host: 'couchdb-develop.net',
+    password: 'password',
+    port: '5984',
+    serviceName: 'couchdb',
+    username: 'user',
+    url: 'http://notInUpsDefinition'
+  },
+  'mysql': {
+    database: 'foo_db',
+    host: 'mysql-develop.net',
+    password: 'password',
+    port: '3306',
+    serviceName: 'mysql',
+    uri: 'mysql://user:password@mysql-develop.net:3306/foo_db',
+    username: 'user'
+  },
+  'no-service': {
+    key: 'value'
+  }
+}
+
 test('index.js', (t) => {
   t.test('Should return default configs if no user services', (tt) => {
-    tt.same(buildConfigs(mockInput), mockInput, 'expect default configs returned')
+    tt.same(buildConfigs(defaultValues), defaultValues, 'expect default configs returned')
     tt.end()
   })
 
   t.test('Should return user services if exists', (tt) => {
-    var buildConfigsMock = rewire('../distribution')
+    let buildConfigsMock = rewire('../distribution')
     buildConfigsMock.__set__('configServices', {
       mysql: {
-        credentials: expectedOutput.mysql
+        credentials: upsDefinitions.mysql
       },
       couchdb: {
-        credentials: expectedOutput.couchdb
+        credentials: upsDefinitions.couchdb
       }
     })
-    tt.same(buildConfigsMock(mockInput), expectedOutput, 'expect user services returned')
+
+    tt.same(buildConfigsMock(defaultValues), expectedOutput, 'expect merged user services returned')
     tt.end()
   })
 
